1f32903b422d22fabb3583fffc8f290ac0eea2d5
Wilson Benavides <benavidesb@unicauca.edu.co>
2018-05-03 03:27:22 -0700

    programatically constraits in setupLayout() function

--- iPadLandscape/ViewController.swift
+++ iPadLandscape/ViewController.swift
@@ -1,82 +1,34 @@
-//
-//  ViewController.swift
-//  iPadLandscape
-//
-//  Created by willix on 4/23/18.
-//  Copyright © 2018 willix. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var bgImage: UIImageView!
-    @IBOutlet weak var lblTitle: UILabel!
-    @IBOutlet weak var btnPlay: UIButton!
-    @IBOutlet weak var btnReset: UIButton!
-    @IBOutlet weak var btnPause: UIButton!
-    var timer = Timer()
-    var counter = 0.0
-    var isRunning = false
+    let jokerImageView: UIImageView = {
+        let imageView = UIImageView(image: #imageLiteral(resourceName: "joker"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print("console message test...")
-        lblTitle.text = "\(counter)"
-        btnPlay.isEnabled = true
-        btnPause.isEnabled = false
+        
+        //view.backgroundColor = .yellow
+        
+        //let imageView = UIImageView(image: #imageLiteral(resourceName: "joker"))
+        view.addSubview(jokerImageView)
+        //imageView.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        setupLayout()
         
         
-        if UIDevice.current.userInterfaceIdiom == .phone { print("iPhone is running") }
-        if UIDevice.current.userInterfaceIdiom == .pad {
-            print("Now iPad is RUNNING...")
-            bgImage.backgroundColor = UIColor(patternImage: UIImage(named: "iPadPortrait01")!)
-        }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
     
-    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
-        print("Landscape Transition?... " + "\(UIDevice.current.orientation.isLandscape)")
-        if UIDevice.current.userInterfaceIdiom == .pad && UIDevice.current.orientation.isLandscape {
-            //print("iPad and Landscape...")
-      
-            bgImage.backgroundColor = UIColor(patternImage: UIImage(named: "iPadLandscape02")!)
-            print("height: " + "\(UIScreen.main.bounds.height)" + ", width: " + "\(UIScreen.main.bounds.width)")
-        }
-        if UIDevice.current.userInterfaceIdiom == .pad && UIDevice.current.orientation.isPortrait {
-            //print("iPad and Landscape...")
-            bgImage.backgroundColor = UIColor(patternImage: UIImage(named: "iPadPortrait01")!)
-        }
+    private func setupLayout() {
+        jokerImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        //imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        jokerImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+        jokerImageView.widthAnchor.constraint(equalToConstant: 250).isActive = true
+        jokerImageView.heightAnchor.constraint(equalToConstant: 250).isActive = true
     }
-    
-    @IBAction func actionPlay(_ sender: UIButton) {
-        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(UpdateTimer), userInfo: nil, repeats: true)
-        btnPlay.isEnabled = false
-        btnPause.isEnabled = true
-        isRunning = true
-    }
-    
-    @objc func UpdateTimer() {
-        counter += 0.1
-        lblTitle.text = String(format: "%.1f", counter)
-    }
-    @IBAction func actionReset(_ sender: UIButton) {
-        timer.invalidate()
-        counter = 0
-        lblTitle.text = "\(counter)"
-        btnPlay.isEnabled = true
-        btnPause.isEnabled = false
-        isRunning = false
-    }
-    @IBAction func actionPause(_ sender: UIButton) {
-        timer.invalidate()
-        btnPlay.isEnabled = true
-        btnPause.isEnabled = false
-    }
-    
 }
 
