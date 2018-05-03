2c02f0c7d1e7a352fe442fa6fcbf80255dcff2f5
Wilson Benavides <benavidesb@unicauca.edu.co>
2018-05-03 09:36:03 -0700

    ep 03 next button and prev button stack view

--- iPadLandscape/ViewController.swift
+++ iPadLandscape/ViewController.swift
@@ -12,9 +12,12 @@ class ViewController: UIViewController {
     let descriptionTextView: UITextView = {
         let textView = UITextView()
         
-        let attributedText = NSMutableAttributedString(string: "Join us today in our fun and games!", attributes: [NSAttributedStringKey.font: UIFont.boldSystemFont(ofSize: 18)])
+        var prop = UIScreen.main.bounds.height / 100.0
+        print("height: " + "\(UIScreen.main.bounds.height)" + ", width: " + "\(UIScreen.main.bounds.width)" + " , related font to 18 and 13: " + "\(prop * 4)" + ", \(prop * 3)")
         
-        attributedText.append(NSAttributedString(string: "\n\n\nAre you ready for loads and loads of fun? Don't wait any longer! We hope to see you in our stores soon.", attributes: [NSAttributedStringKey.font: UIFont.systemFont(ofSize: 13), NSAttributedStringKey.foregroundColor: UIColor.gray]))
+        let attributedText = NSMutableAttributedString(string: "Join us today in our fun and games!", attributes: [NSAttributedStringKey.font: UIFont.boldSystemFont(ofSize: prop * 4)])
+        
+        attributedText.append(NSAttributedString(string: "\n\n\nAre you ready for loads and loads of fun? Don't wait any longer! We hope to see you in our stores soon.", attributes: [NSAttributedStringKey.font: UIFont.systemFont(ofSize: prop * 3), NSAttributedStringKey.foregroundColor: UIColor.gray]))
         
         textView.attributedText = attributedText
         
@@ -29,7 +32,15 @@ class ViewController: UIViewController {
     
     private let previousButton: UIButton = {
         let button = UIButton(type: .system)
-        button.setTitle("Prev", for: .normal)
+        button.setTitle("PREV", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        return button
+    }()
+    
+    let nextButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("NEXT", for: .normal)
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
@@ -39,7 +50,7 @@ class ViewController: UIViewController {
         print("console message test...")
         if UIDevice.current.userInterfaceIdiom == .phone {
             print("height: " + "\(UIScreen.main.bounds.height)" + ", width: " + "\(UIScreen.main.bounds.width)" + " , iPhone device.")
-        } else if UIDevice.current.userInterfaceIdiom == .phone {
+        } else if UIDevice.current.userInterfaceIdiom == .pad {
             print("height: " + "\(UIScreen.main.bounds.height)" + ", width: " + "\(UIScreen.main.bounds.width)" + " , iPad device.")
         }
         view.addSubview(descriptionTextView)
@@ -48,16 +59,40 @@ class ViewController: UIViewController {
     }
     
     fileprivate func setupBottomControls() {
-        view.addSubview(previousButton)
+        //view.addSubview(previousButton)
         previousButton.backgroundColor = .red
         //previousButton.frame = CGRect(x: 0, y: 0, width: 200, height: 50)
         
-        NSLayoutConstraint.activate([
-            previousButton.topAnchor.constraint(equalTo: view.topAnchor),
-            previousButton.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            previousButton.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            previousButton.heightAnchor.constraint(equalToConstant: 50)
-            ])
+        //let yellowView = UIView()
+        //yellowView.backgroundColor = .yellow
+        
+        let greenView = UIView()
+        greenView.backgroundColor = .green
+        
+        //let blueView = UIView()
+        //blueView.backgroundColor = .blue
+        
+        let bottomControlsStackView = UIStackView(arrangedSubviews: [previousButton, greenView, nextButton])
+        bottomControlsStackView.translatesAutoresizingMaskIntoConstraints = false
+        bottomControlsStackView.distribution = .fillEqually
+        //bottomControlsStackView.axis = .vertical
+        view.addSubview(bottomControlsStackView)
+        
+        if #available(iOS 11.0, *) {
+            NSLayoutConstraint.activate([
+                bottomControlsStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+                bottomControlsStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+                bottomControlsStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+                bottomControlsStackView.heightAnchor.constraint(equalToConstant: 50)
+                ])
+        } else {
+            NSLayoutConstraint.activate([
+                bottomControlsStackView.bottomAnchor.constraint(equalTo: self.bottomLayoutGuide.topAnchor),
+                bottomControlsStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                bottomControlsStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+                bottomControlsStackView.heightAnchor.constraint(equalToConstant: 50)
+                ])
+        }
     }
     
     private func setupLayout() {
