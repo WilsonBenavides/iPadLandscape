8f1dcd4f994b00bf884f00b29847c73c84c695ea
Wilson Benavides <benavidesb@unicauca.edu.co>
2018-05-03 04:58:59 -0700

    ep 01 manual layout constrait ios

--- iPadLandscape/ViewController.swift
+++ iPadLandscape/ViewController.swift
@@ -8,27 +8,35 @@ class ViewController: UIViewController {
         return imageView
     }()
     
+    let descriptionTextView: UITextView = {
+        let textView = UITextView()
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.text = "Join us today in our fun and games!"
+        textView.font = UIFont.boldSystemFont(ofSize: 18)
+        textView.textAlignment = .center
+        textView.isEditable = false
+        textView.isScrollEnabled = false
+        return textView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("console message test...")
-        
-        //view.backgroundColor = .yellow
-        
-        //let imageView = UIImageView(image: #imageLiteral(resourceName: "joker"))
         view.addSubview(jokerImageView)
-        //imageView.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        view.addSubview(descriptionTextView)
         setupLayout()
-        
-        
-        
     }
     
     private func setupLayout() {
         jokerImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        //imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         jokerImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
-        jokerImageView.widthAnchor.constraint(equalToConstant: 250).isActive = true
-        jokerImageView.heightAnchor.constraint(equalToConstant: 250).isActive = true
+        jokerImageView.widthAnchor.constraint(equalToConstant: 159).isActive = true
+        jokerImageView.heightAnchor.constraint(equalToConstant: 184.5).isActive = true
+        
+        descriptionTextView.topAnchor.constraint(equalTo: jokerImageView.bottomAnchor, constant: 100).isActive = true
+        descriptionTextView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        descriptionTextView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        descriptionTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
     }
 }
 
