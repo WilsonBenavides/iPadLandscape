fe81950cf04055b1e31300e69b37a6c1c88f34b0
Wilson Benavides <benavidesb@unicauca.edu.co>
2018-05-03 08:26:46 -0700

    ep 03 top width constrait section

--- iPadLandscape/ViewController.swift
+++ iPadLandscape/ViewController.swift
@@ -11,28 +11,58 @@ class ViewController: UIViewController {
     
     let descriptionTextView: UITextView = {
         let textView = UITextView()
+        
+        let attributedText = NSMutableAttributedString(string: "Join us today in our fun and games!", attributes: [NSAttributedStringKey.font: UIFont.boldSystemFont(ofSize: 18)])
+        
+        attributedText.append(NSAttributedString(string: "\n\n\nAre you ready for loads and loads of fun? Don't wait any longer! We hope to see you in our stores soon.", attributes: [NSAttributedStringKey.font: UIFont.systemFont(ofSize: 13), NSAttributedStringKey.foregroundColor: UIColor.gray]))
+        
+        textView.attributedText = attributedText
+        
         textView.translatesAutoresizingMaskIntoConstraints = false
-        textView.text = "Join us today in our fun and games!"
-        textView.font = UIFont.boldSystemFont(ofSize: 18)
+        //textView.text = "Join us today in our fun and games!"
+        //textView.font = UIFont.boldSystemFont(ofSize: 18)
         textView.textAlignment = .center
         textView.isEditable = false
         textView.isScrollEnabled = false
         return textView
     }()
     
+    private let previousButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Prev", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("console message test...")
-        //view.addSubview(jokerImageView)
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            print("height: " + "\(UIScreen.main.bounds.height)" + ", width: " + "\(UIScreen.main.bounds.width)" + " , iPhone device.")
+        } else if UIDevice.current.userInterfaceIdiom == .phone {
+            print("height: " + "\(UIScreen.main.bounds.height)" + ", width: " + "\(UIScreen.main.bounds.width)" + " , iPad device.")
+        }
         view.addSubview(descriptionTextView)
+        setupBottomControls()
         setupLayout()
+    }
+    
+    fileprivate func setupBottomControls() {
+        view.addSubview(previousButton)
+        previousButton.backgroundColor = .red
+        //previousButton.frame = CGRect(x: 0, y: 0, width: 200, height: 50)
+        
+        NSLayoutConstraint.activate([
+            previousButton.topAnchor.constraint(equalTo: view.topAnchor),
+            previousButton.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            previousButton.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            previousButton.heightAnchor.constraint(equalToConstant: 50)
+            ])
     }
     
     private func setupLayout() {
         let topImageContainerView = UIView()
-        topImageContainerView.backgroundColor = .blue
         view.addSubview(topImageContainerView)
-        //topImageContainerView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
         topImageContainerView.translatesAutoresizingMaskIntoConstraints = false
         topImageContainerView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
         topImageContainerView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
@@ -41,20 +71,15 @@ class ViewController: UIViewController {
         topImageContainerView.addSubview(jokerImageView)
         jokerImageView.centerXAnchor.constraint(equalTo: topImageContainerView.centerXAnchor).isActive = true
         jokerImageView.centerYAnchor.constraint(equalTo: topImageContainerView.centerYAnchor).isActive = true
-        jokerImageView.heightAnchor.constraint(equalTo: topImageContainerView.heightAnchor, multiplier: 0.5).isActive = true
+        jokerImageView.heightAnchor.constraint(equalTo: topImageContainerView.heightAnchor, multiplier: 0.7).isActive = true
         
         //topImageContainerView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
         //topImageContainerView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
         topImageContainerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5).isActive = true
         
-        //jokerImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        //jokerImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
-        //jokerImageView.widthAnchor.constraint(equalToConstant: 159).isActive = true
-        //jokerImageView.heightAnchor.constraint(equalToConstant: 184.5).isActive = true
-        
-        descriptionTextView.topAnchor.constraint(equalTo: jokerImageView.bottomAnchor, constant: 100).isActive = true
-        descriptionTextView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-        descriptionTextView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        descriptionTextView.topAnchor.constraint(equalTo: topImageContainerView.bottomAnchor).isActive = true
+        descriptionTextView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 24).isActive = true
+        descriptionTextView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -24).isActive = true
         descriptionTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
     }
 }
