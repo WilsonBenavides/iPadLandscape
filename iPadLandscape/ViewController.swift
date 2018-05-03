cceaa0c3992d2e9819ec5dc956a45eb03bbab581
Wilson Benavides <benavidesb@unicauca.edu.co>
2018-05-03 06:23:23 -0700

    correct display anchor image auto layout

--- iPadLandscape/ViewController.swift
+++ iPadLandscape/ViewController.swift
@@ -23,7 +23,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("console message test...")
-        view.addSubview(jokerImageView)
+        //view.addSubview(jokerImageView)
         view.addSubview(descriptionTextView)
         setupLayout()
     }
@@ -35,14 +35,22 @@ class ViewController: UIViewController {
         //topImageContainerView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
         topImageContainerView.translatesAutoresizingMaskIntoConstraints = false
         topImageContainerView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-        topImageContainerView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-        topImageContainerView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        topImageContainerView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        topImageContainerView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        
+        topImageContainerView.addSubview(jokerImageView)
+        jokerImageView.centerXAnchor.constraint(equalTo: topImageContainerView.centerXAnchor).isActive = true
+        jokerImageView.centerYAnchor.constraint(equalTo: topImageContainerView.centerYAnchor).isActive = true
+        jokerImageView.heightAnchor.constraint(equalTo: topImageContainerView.heightAnchor, multiplier: 0.5).isActive = true
+        
+        //topImageContainerView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        //topImageContainerView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
         topImageContainerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5).isActive = true
         
-        jokerImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        jokerImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
-        jokerImageView.widthAnchor.constraint(equalToConstant: 159).isActive = true
-        jokerImageView.heightAnchor.constraint(equalToConstant: 184.5).isActive = true
+        //jokerImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        //jokerImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+        //jokerImageView.widthAnchor.constraint(equalToConstant: 159).isActive = true
+        //jokerImageView.heightAnchor.constraint(equalToConstant: 184.5).isActive = true
         
         descriptionTextView.topAnchor.constraint(equalTo: jokerImageView.bottomAnchor, constant: 100).isActive = true
         descriptionTextView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
