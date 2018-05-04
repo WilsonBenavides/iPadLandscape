6c9101a20bba4f4e9210b7a59f15ad1bc6312372
Wilson Benavides <benavidesb@unicauca.edu.co>
2018-05-03 22:06:14 -0700

    ep 04 purple background

--- iPadLandscape/AppDelegate.swift
+++ iPadLandscape/AppDelegate.swift
@@ -15,7 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        window = UIWindow()
+        window?.makeKeyAndVisible()
+        
+        let randomViewController = UIViewController()
+        randomViewController.view.backgroundColor = .purple
+        window?.rootViewController = randomViewController
+        
         return true
     }
 
---
+++ iPadLandscape/SwipingController.swift
@@ -0,0 +1,18 @@
+//
+//  SwipingController.swift
+//  iPadLandscape
+//
+//  Created by willix on 3/05/18.
+//  Copyright © 2018 willix. All rights reserved.
+//
+
+import UIKit
+
+class SwipingController: UICollectionViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView?.backgroundColor = .green
+    }
+}
